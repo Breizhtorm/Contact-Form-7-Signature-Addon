253589817263f00633d64a24d21d476ff2176a85
Jerome <jerome@breizhtorm.fr>
2017-04-04 16:15:54 +0200

    Switching to jQuery and using a global init method

--- scripts.js
+++ scripts.js
@@ -1,54 +1,48 @@
-var signatures = [];
+(function($) {
 
-//window.onresize = sigFieldsResize;
+	'use strict';
 
-document.addEventListener("DOMContentLoaded", function(){
+	var signatures = [];
 
-	var forms = document.querySelectorAll("form.wpcf7-form");
-	Array.prototype.forEach.call(forms, function(form, i){
+	// Main function to initialize signature fields
+	$.fn.wpcf7InitSignatures = function() {
 
-		var wrappers = document.querySelectorAll(".wpcf7-form-control-signature-global-wrap");
-		Array.prototype.forEach.call(wrappers, function(wrapper, i){
+		var submit = this.find('input.wpcf7-submit')[0];
 
-			var canvas = wrapper.querySelector("canvas");
-			var clear = wrapper.querySelector("input[type=button]");
-			var submit = form.querySelector("input.wpcf7-submit");
+		this.find('.wpcf7-form-control-signature-global-wrap').each(function(i){
+			var $canvas = $(this).find('canvas'), 
+				$clear = $(this).find('input[type=button]'),
+				id = $(this).data('field-id'),
+				input = document.getElementById("wpcf7_input_" + id),
+				canvas = $canvas[0];
 
-			var id = wrapper.getAttribute("data-field-id");
-			var input = document.getElementById("wpcf7_input_" + id);
-
-			// Resize field (for pixel ratio issues)
 			sigFieldRatio(canvas);
 
 			// Options
 			var options = [];
-			if (canvas.hasAttribute("data-color")){
-				options['penColor'] = canvas.getAttribute("data-color");
-			}
-			if (canvas.hasAttribute("data-background")){
-				options['backgroundColor'] = canvas.getAttribute("data-background");
-			}
+			options['penColor'] = $canvas.data("color");
+			options['backgroundColor'] = $canvas.data("background");
 
 			// Canvas init
 			var signature = new SignaturePad(canvas, options);
 
-			// Push field elements into global var
-			signatures.push({
+			// Push field elements into global variable
+			signatures[i] = {
 				signature: signature, 
 				input: input, 
 				canvas: canvas, 
 				options: options
-			});
+			};
 
-			sigFieldInit(i);
+			sigFieldSetValue(i);
 
 			// Clear event listener
-			clear.addEventListener("click", function(){
+			$clear.on("click", function(){
 				sigFieldClear(i);
 			});
 
 			// Trigger change event on input field when signature changed
-			canvas.addEventListener("mouseup", function(){
+			$clear.on("mouseup", function(){
 				sigFieldChange(i);
 			});
 
@@ -58,88 +52,95 @@ document.addEventListener("DOMContentLoaded", function(){
 					sigFieldBeforeSubmit(i);
 				}, false);
 			}
+
 		});
-	});
-});
 
-// Init Canvas value if needed
-function sigFieldInit(index){
-	
-	if(signatures[index].input.value != ''){
-		signatures[index].signature.fromDataURL(signatures[index].input.value);
+		return this;
 	}
-}
 
-// Trigger Change event
-function sigFieldChange(index){
 
-	sigFieldBeforeSubmit(index);
-	
-	if (document.createEvent) {
-		var changeEvent = document.createEvent("HTMLEvents");
-	    changeEvent.initEvent("change", false, true);
-	    signatures[index].input.dispatchEvent(changeEvent);
-	} else {
-		signatures[index].input.fireEvent("onchange");
+	// Resize canvas fields
+	$.fn.sigFieldsResize = function(){
+
+		$(".wpcf7-form-control-signature-input-wrap").each(function(i){
+			sigFieldResize(i, true);
+		});
+
+		return this;
 	}
-}
 
-// Copy sig value to input field
-function sigFieldBeforeSubmit(index){
-	if (!signatures[index].signature.isEmpty()){
-		signatures[index].input.value = signatures[index].signature.toDataURL();
-	}else{
-		signatures[index].input.value = "";
-	}
-}
-
-// Clear a single signature field
-function sigFieldClear(index){
-	
-	signatures[index].signature.clear();
-	signatures[index].input.value= "";
-}
-
-// Dealing with window size and device ratio
-function sigFieldRatio(canvas){
-
-	var ratio =  Math.max(window.devicePixelRatio || 1, 1);
-    canvas.width = canvas.offsetWidth * ratio;
-    canvas.height = canvas.offsetHeight * ratio;
-    canvas.getContext("2d").scale(ratio, ratio);
-
-}
-
-// Dealing with window size and device ratio
-function sigFieldResize(index, clear){
-
-	var canvas = signatures[index].canvas;
-
-	sigFieldRatio(canvas)
-
-    if (clear){
-    	sigFieldClear(index);
-    }
-}
-
-// Global resize management
-function sigFieldsResize(){
-
-	var elements = document.querySelectorAll(".wpcf7-form-control-signature-input-wrap");
-	Array.prototype.forEach.call(elements, function(el, i){
-
-		sigFieldResize(i, true);
-
-	});
-}
-
-// Globally clear fields on form submit
-function sigFieldsClear(){
-	
-	var elements = document.querySelectorAll(".wpcf7-form-control-signature-input-wrap");
-	Array.prototype.forEach.call(elements, function(el, i){
+	// Globally clear fields (on form submit for exemple)
+	$.fn.sigFieldsClear = function(){
 		
-		sigFieldClear(i);
+		$(".wpcf7-form-control-signature-input-wrap").each(function(i){
+			sigFieldClear(i);
+		});
 
+		return this;
+	}
+
+	// Set field ratio
+    function sigFieldRatio( canvas ) {
+
+    	var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+	    canvas.width = canvas.offsetWidth * ratio;
+	    canvas.height = canvas.offsetHeight * ratio;
+	    canvas.getContext("2d").scale(ratio, ratio);
+    };
+
+	// Set Canvas value if needed
+	function sigFieldSetValue( index ){
+		
+		if(signatures[index].input.value != ''){
+			signatures[index].signature.fromDataURL(signatures[index].input.value);
+		}
+	}
+
+	// Trigger Change event
+	function sigFieldChange( index ){
+
+		sigFieldBeforeSubmit(index);
+		
+		if (document.createEvent) {
+			var changeEvent = document.createEvent("HTMLEvents");
+		    changeEvent.initEvent("change", false, true);
+		    signatures[index].input.dispatchEvent(changeEvent);
+		} else {
+			signatures[index].input.fireEvent("onchange");
+		}
+	}
+
+	// Copy sig value to input field
+	function sigFieldBeforeSubmit( index ){
+
+		if (!signatures[index].signature.isEmpty()){
+			signatures[index].input.value = signatures[index].signature.toDataURL();
+		}else{
+			signatures[index].input.value = "";
+		}
+	}
+
+	// Clear a single signature field
+	function sigFieldClear( index ){
+
+		signatures[index].signature.clear();
+		signatures[index].input.value= "";
+	}
+
+	// Dealing with window size and device ratio
+	function sigFieldResize( index, clear ){
+
+		var canvas = signatures[index].canvas;
+
+		sigFieldRatio(canvas);
+
+	    if (clear){
+	    	sigFieldClear(index);
+	    }
+	}
+
+	$(function() {
+		$('div.wpcf7 > form').wpcf7InitSignatures();
 	});
-}
+
+})(jQuery);
